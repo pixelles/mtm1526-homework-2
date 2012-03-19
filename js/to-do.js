a7535dcde4e72dcd941f96b1148d21f8fccb440a
pixelles <patriciadupelle@msn.com>
2012-03-18 20:13:00 -0400

    Added an event listener to ordered list to give the class name of 'done' when clicked.

--- js/to-do.js
+++ js/to-do.js
@@ -1,5 +1,6 @@
 var task = document.getElementById('task'); 
 var list = document.getElementById('list')
+
 document.getElementById('add-task').addEventListener('click', function (ev) {
 	
 	if (task.value != '') {
@@ -10,4 +11,10 @@ document.getElementById('add-task').addEventListener('click', function (ev) {
 	task.value = '';
 }, false);
 
-
+list.addEventListener('click', function (ev) {
+  if (ev.target.className == 'done') {
+		ev.target.className = '';
+	 } else {
+		ev.target.className = 'done';
+	 }
+}, false);
